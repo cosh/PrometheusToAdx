b13eb2eb5b82681095f335371553f544a4e31dec
Henning Rauch <herauch@microsoft.com>
2019-07-09 16:38:11 +0200

    writing async messages to eventhub

--- PrometheusWrite/Write.cs
+++ PrometheusWrite/Write.cs
@@ -1,32 +1,33 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Snappy;
-using System.IO.Compression;
+using Prometheus;
+using Newtonsoft.Json;
 
 namespace PrometheusWrite
 {
     public static class Write
     {
         [FunctionName("Write")]
-        [return: EventHub("outputEventHubMessage", Connection = "EventHubConnectionAppSetting")]
-        public static String Run(
+        public static async Task Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [EventHub("dest", Connection = "EventHubConnectionAppSetting")]IAsyncCollector<string> outputEvents,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             var decompressed = DecompressBody(req.Body);
 
-            return "testteerst";
+            var writerequest = WriteRequest.Parser.ParseFrom(decompressed);
+
+            foreach (var aTimeseries in writerequest.Timeseries)
+            {
+                await outputEvents.AddAsync(JsonConvert.SerializeObject(aTimeseries));
+            }
         }
 
-        private static String DecompressBody(Stream body)
+        private static byte[] DecompressBody(Stream body)
         {
             MemoryStream ms = new MemoryStream();
             body.CopyTo(ms);
@@ -36,7 +37,7 @@ namespace PrometheusWrite
 
             var decompressed = decompressor.Decompress(source, 0, source.Length);
 
-            return System.Text.Encoding.UTF8.GetString(decompressed);
+            return decompressed;
         }
     }
 }
