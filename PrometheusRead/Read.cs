b4aefd9648237a95c835099203f1c257743985bc
Henning Rauch <herauch@microsoft.com>
2019-07-10 13:57:38 +0200

    some more boilerplate

--- PrometheusRead/Read.cs
+++ PrometheusRead/Read.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,23 +10,42 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PrmoetheusHelper.Helper;
 using Prometheus;
+using Snappy.Sharp;
 
 namespace PrometheusRead
 {
     public static class Read
     {
+        private static SnappyCompressor compressor = new SnappyCompressor();
+
         [FunctionName("Read")]
-        public static async Task<ReadResponse> Run(
+        public static byte[] Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             var decompressed = Conversion.DecompressBody(req.Body);
-
             var readrequest = ReadRequest.Parser.ParseFrom(decompressed);
 
+            ReadResponse response = CreateResponse(readrequest);
 
+            MemoryStream ms = new MemoryStream();
+            response.WriteTo(new Google.Protobuf.CodedOutputStream(ms));
 
-            return null;
+            var resultUncompressed = ms.ToArray();
+
+            //should be at least the size of the uncompressed one
+            byte[] resultCompressed = new byte[resultUncompressed.Length];
+
+            var compressedSize = compressor.Compress(resultUncompressed, 0, resultUncompressed.Length, resultCompressed);
+
+            byte[] resultCompressedShortened = resultCompressed.Take(compressedSize).ToArray();
+
+            return resultCompressedShortened;
+        }
+
+        private static ReadResponse CreateResponse(ReadRequest readrequest)
+        {
+            throw new NotImplementedException();
         }
     }
 }
